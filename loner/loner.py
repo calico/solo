f6e5b2fc8c20bb5b3ffb3752c9cc06c589f0f460
njbernstein <njbernstein@gmail.com>
2019-07-23 08:25:57 -0700

    removed dot from utils

--- loner/loner.py
+++ loner/loner.py
@@ -16,7 +16,7 @@ from scvi.models import Classifier, VAE
 from scvi.inference import UnsupervisedTrainer, ClassifierTrainer
 import torch
 
-from .utils import create_average_doublet, create_summed_doublet, \
+from utils import create_average_doublet, create_summed_doublet, \
     create_multinomial_doublet, make_gene_expression_dataset
 
 '''
