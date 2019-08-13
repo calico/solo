f5adf4464d92c95d48a0ac93d5472c06adc16734
njbernstein <njbernstein@gmail.com>
2019-08-13 12:58:08 -0700

    more conflicts to fix

--- loner/__init__.py
+++ loner/__init__.py
@@ -2,8 +2,4 @@ __author__ = 'David Kelley, Nick Bernstein'
 __email__ = 'njbernstein@gmail.com'
 __version__ = '0.1'
 
-<<<<<<< HEAD
 from . import utils
-=======
-from . import utils
->>>>>>> eaae29f7e1fcfc0033b0a2bbf273b551d060eea0
