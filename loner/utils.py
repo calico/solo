2ed598d35c8d525627feaba04e7416e2ed1fd90e
njbernstein <njbernstein@gmail.com>
2019-08-19 11:20:44 -0700

    hopefully a better fix to the hack for multinomial issues

--- loner/utils.py
+++ loner/utils.py
@@ -21,20 +21,23 @@ def create_multinomial_doublet(X, i, j, **kwargs):
     dp = (X[i, :]
           + X[j, :]).astype('float64')
 
+    # a huge hack caused by
+    # https://github.com/numpy/numpy/issues/8317
+    # fun fun fun https://stackoverflow.com/questions/23257587/how-can-i-avoid-value-errors-when-using-numpy-random-multinomial
+    # okay with this hack because affects pro
+    non_zero_indexes = np.where(dp > 0)[0]
+    dp = dp[non_zero_indexes]
     # normalize
     dp /= dp.sum()
 
-    # a huge hack caused by
-    # fun fun fun https://stackoverflow.com/questions/23257587/how-can-i-avoid-value-errors-when-using-numpy-random-multinomial
-    # okay with this hack because affects pro
-    off_zero = (dp.sum() - 1)
-    if off_zero != 0:
-        dp[np.where(dp > 0)[0][0]] = dp[dp > 0][0] - (off_zero)
     # choose depth
     dd = int(doublet_depth * (cell_depths[i] + cell_depths[j]) / 2)
 
     # sample counts from multinomial
-    return multinomial.rvs(n=dd, p=dp)
+    non_zero_probs = multinomial.rvs(n=dd, p=dp)
+    probs = np.zeros(X.shape[1])
+    probs[non_zero_indexes] = non_zero_probs
+    return probs
 
 
 def make_gene_expression_dataset(data, gene_names):
