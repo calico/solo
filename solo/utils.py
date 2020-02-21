26476a94f52e1b9c8c66db3db2d3811ff27763ac
njbernstein <njbernstein@gmail.com>
2020-02-21 14:48:50 -0800

    adding randomizing depth option

--- solo/utils.py
+++ solo/utils.py
@@ -2,6 +2,73 @@ import numpy as np
 
 from scvi.dataset import GeneExpressionDataset
 from scipy.stats import multinomial
+from sklearn.neighbors import NearestNeighbors
+
+
+def knn_smooth_pred_class(X: np.ndarray,
+                          pred_class: np.ndarray,
+                          grouping: np.ndarray = None,
+                          k: int = 15,) -> np.ndarray:
+    '''
+    Smooths class predictions by taking the modal class from each cell's
+    nearest neighbors.
+    Parameters
+    ----------
+    X : np.ndarray
+        [N, Features] embedding space for calculation of nearest neighbors.
+    pred_class : np.ndarray
+        [N,] array of unique class labels.
+    groupings : np.ndarray
+        [N,] unique grouping labels for i.e. clusters.
+        if provided, only considers nearest neighbors *within the cluster*.
+    k : int
+        number of nearest neighbors to use for smoothing.
+    Returns
+    -------
+    smooth_pred_class : np.ndarray
+        [N,] unique class labels, smoothed by kNN.
+    Examples
+    --------
+    >>> smooth_pred_class = knn_smooth_pred_class(
+    ...     X = X,
+    ...     pred_class = raw_predicted_classes,
+    ...     grouping = louvain_cluster_groups,
+    ...     k = 15,)
+    Notes
+    -----
+    scNym classifiers do not incorporate neighborhood information.
+    By using a simple kNN smoothing heuristic, we can leverage neighborhood
+    information to improve classification performance, smoothing out cells
+    that have an outlier prediction relative to their local neighborhood.
+    '''
+    if grouping is None:
+        # do not use a grouping to restrict local neighborhood
+        # associations, create a universal pseudogroup `0`.
+        grouping = np.zeros(X.shape[0])
+
+    smooth_pred_class = np.zeros_like(pred_class)
+    for group in np.unique(grouping):
+        # identify only cells in the relevant group
+        group_idx = np.where(grouping == group)[0].astype('int')
+        X_group = X[grouping == group, :]
+        # if there are < k cells in the group, change `k` to the
+        # group size
+        if X_group.shape[0] < k:
+            k_use = X_group.shape[0]
+        else:
+            k_use = k
+        # compute a nearest neighbor graph and identify kNN
+        nns = NearestNeighbors(n_neighbors=k_use,).fit(X_group)
+        dist, idx = nns.kneighbors(X_group)
+
+        # for each cell in the group, assign a class as
+        # the majority class of the kNN
+        for i in range(X_group.shape[0]):
+            classes = pred_class[group_idx[idx[i, :]]]
+            uniq_classes, counts = np.unique(classes, return_counts=True)
+            maj_class = uniq_classes[int(np.argmax(counts))]
+            smooth_pred_class[group_idx[i]] = maj_class
+    return smooth_pred_class
 
 
 def create_average_doublet(X: np.ndarray,
@@ -72,6 +139,8 @@ def create_multinomial_doublet(X: np.ndarray,
     doublet_depth = kwargs["doublet_depth"]
     cell_depths = kwargs["cell_depths"]
     cells_ids = kwargs["cells_ids"]
+    randomize_doublet_size = kwargs["randomize_doublet_size"]
+
     # add their counts
     dp = (X[i, :]
           + X[j, :]).astype('float64')
@@ -84,9 +153,12 @@ def create_multinomial_doublet(X: np.ndarray,
     dp = dp[non_zero_indexes]
     # normalize
     dp /= dp.sum()
-
+    if randomize_doublet_size:
+        scale_factor = np.random.uniform(1., doublet_depth)
+    else:
+        scale_factor = doublet_depth
     # choose depth
-    dd = int(doublet_depth * (cell_depths[i] + cell_depths[j]) / 2)
+    dd = int(scale_factor * (cell_depths[i] + cell_depths[j]) / 2)
 
     # sample counts from multinomial
     non_zero_probs = multinomial.rvs(n=dd, p=dp)
