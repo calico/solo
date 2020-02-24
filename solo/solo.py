c76eed2fdd9ccf03aa00e561d9fe140c790fd48e
njbernstein <njbernstein@gmail.com>
2020-02-24 08:13:08 -0800

    adding randomize keyword

--- solo/solo.py
+++ solo/solo.py
@@ -83,7 +83,7 @@ def main():
     parser.add_argument('-l', dest='normal_logging',
                         default=False, action='store_true',
                         help='Logging level set to normal (aka not debug)')
-    parser.add_argument('--random_size', dest='randomize_doublet_size',
+    parser.add_argument('--random-size', dest='randomize_doublet_size',
                         default=False,
                         action='store_true',
                         help='Sample depth multipliers from Unif(1, \
@@ -257,7 +257,8 @@ def main():
         in_silico_doublets[di, :] = \
             doublet_function(singlet_scvi_data.X, i, j,
                              doublet_depth=args.doublet_depth,
-                             cell_depths=cell_depths, cells_ids=cells_ids)
+                             cell_depths=cell_depths, cells_ids=cells_ids,
+                             randomize_doublet_size=randomize_doublet_size)
 
     # merge datasets
     # we can maybe up sample the known doublets
